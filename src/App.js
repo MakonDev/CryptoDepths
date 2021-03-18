fad11595cdc6d173f3713325ee4d775233d9fae0
NickSadler2018 <nicksadler14@gmail.com>
2021-03-17 22:36:09 -0400

    frame for binance added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+// import Binance from 'binance-api-node'
 
-function App() {
+// const client = Binance()
+
+// const client2 = Binance({
+//   apiKey: process.env.REACT_APP_apiKey,
+//   apiSecret: process.env.REACT_APP_apiSecret
+// })
+
+function App () {
+  // client.ws.partialDepth({ symbol: 'ETHUSD', level: 10 }, depth => {
+  //   console.log(depth)
+  // })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
